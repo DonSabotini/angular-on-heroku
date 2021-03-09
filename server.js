7351871ddf12ca2a2c36f8975aec766ff1ed9359
DonSabotini <71654802+DonSabotini@users.noreply.github.com>
2021-03-10 00:28:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-on-heroku'));
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/team-three-frontend'));
+
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/angular-on-heroku/index.html'));});
+    
+res.sendFile(path.join(__dirname,'/dist/team-three-frontend/index.html'));
+});
+
+console.log(path);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
